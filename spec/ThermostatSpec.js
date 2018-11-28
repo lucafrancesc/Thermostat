6522bebcfbbe62b3dd1bb5124ab4c8eefe802172
Luca <eto.lucafrancesco@gmail.com>
2018-11-28 18:44:33 +0000

    Saving Mode on/off implemented

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,35 +8,82 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  it('its starting temperature is 20', function() {
-    expect(thermostat.currentTemperature()).toEqual(20)
+  describe('initial settings', function(){
+    it('its starting temperature is 20', function() {
+      expect(thermostat.currentTemperature()).toEqual(20)
+    });
+
+    it('its min temperature is 10', function() {
+      expect(thermostat.minTemperature()).toEqual(10)
+    });
   });
 
-  it('its min temperature is 10', function() {
-    expect(thermostat.minTemperature()).toEqual(10)
+  describe('saving mode on', function(){
+    it('energy saving mode on by default', function() {
+      expect(thermostat.powerMode()).toEqual(thermostat.savingModeOn())
+    });
+
+    it('energy saving mode on', function() {
+      expect(thermostat.savingModeOn()).toEqual('On')
+    });
+
+    it('it can\'t increase the temperature above 25', function() {
+      expect(thermostat.maxTemperature()).toEqual(25)
+    });
+
+    it('it can\'t increase the temperature above 25', function() {
+      for(var i = 1; i < 5; i++) { thermostat.up(); }
+      thermostat.up();
+      expect(thermostat.currentTemperature()).toEqual(25)
+    });
   });
 
-  it('raises the temperature', function() {
-    thermostat.up();
-    expect(thermostat.currentTemperature()).toEqual(21)
+  describe('saving mode off', function(){
+    it('energy saving mode off', function() {
+      expect(thermostat.savingModeOff()).toEqual('Off')
+    });
+
+    it('energy saving mode off', function() {
+      thermostat.savingModeOff()
+      expect(thermostat.maxTemperature()).toEqual(32)
+    });
+
+    it('it can\'t increase the temperature above 32', function() {
+      thermostat.savingModeOff()
+      for(var i = 1; i < 12; i++) { thermostat.up(); }
+      thermostat.up();
+      expect(thermostat.currentTemperature()).toEqual(32)
+    });
   });
 
-  it('decrease the temperature', function() {
-    thermostat.down();
-    expect(thermostat.currentTemperature()).toEqual(19)
+  describe('reset', function(){
+    it('resets the temperature to 20', function() {
+      thermostat.up();
+      thermostat.reset();
+      expect(thermostat.currentTemperature()).toEqual(20)
+    });
   });
 
-  it('resets the temperature to 20', function() {
-    thermostat.up();
-    thermostat.reset();
-    expect(thermostat.currentTemperature()).toEqual(20)
+  describe('increase the temperature', function(){
+    it('raises the temperature', function() {
+      thermostat.up();
+      expect(thermostat.currentTemperature()).toEqual(21)
+    });
   });
 
-  it('decrease the temperature', function() {
-    for(var i = 1; i < 10; i++) { thermostat.down(); }
-    thermostat.down();
-    expect(thermostat.currentTemperature()).toEqual(10)
+  describe('decrease the temp until the min', function(){
+    it('decrease the temperature by one', function() {
+      thermostat.down();
+      expect(thermostat.currentTemperature()).toEqual(19)
+    });
+
+    it('it can\'t decrease the temperature below 10', function() {
+      for(var i = 1; i < 10; i++) { thermostat.down(); }
+      thermostat.down();
+      expect(thermostat.currentTemperature()).toEqual(10)
+    });
   });
-
-
 });
+// describe('', function(){
+//
+// });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,23 +3,60 @@
 function Thermostat() {
   this.temperature = 20
   this.minTemp = 10
-}
+  this.savingMode = 'On'
+  this.maxTemp = 25
 
+}
+// TEMPERATURE
 Thermostat.prototype.minTemperature = function () {
   return this.minTemp
+};
+
+Thermostat.prototype.maxTemperature = function () {
+  return this.maxTemp
 };
 
 Thermostat.prototype.currentTemperature = function () {
   return this.temperature
 };
 
+// MODES
+Thermostat.prototype.powerMode = function () {
+  return this.savingMode
+};
+
+Thermostat.prototype.savingModeOn = function () {
+  this.maxTemp = 25
+  return this.savingMode = 'On'
+};
+
+Thermostat.prototype.savingModeOff = function () {
+  this.maxTemp = 32
+  return this.savingMode = 'Off'
+};
+
+// TEMPERATURE REGULATORS
 Thermostat.prototype.up = function () {
-  return this.temperature++
+  if (this.savingMode === 'On') {
+    if (this.temperature < this.maxTemp) {
+      this.temperature++;
+      return this.temperature;
+    } else {
+      return this.maxTemp;
+    }
+  } else if (this.savingMode === 'Off') {
+    if (this.temperature < this.maxTemp) {
+      this.temperature++;
+      return this.temperature;
+    } else {
+      return this.maxTemp;
+    }
+  }
 };
 
 Thermostat.prototype.down = function () {
   if (this.temperature > this.minTemp) {
-    return this.temperature--
+    this.temperature--
   } else {
     return this.minTemp
   }
